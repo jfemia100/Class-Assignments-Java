5a06bdff3f00ffa537ee9c1e44ca5529b85a917a
Joseph Femia <josephfemia@yahoo.com>
2017-03-30 22:05:44 -0400

    Create TextFileInput.java

---
+++ TextFileInput.java
@@ -0,0 +1,57 @@
+//  *** This was not created by me. 
+
+//      This is a class that was created by one of the professors in my school
+//      and we are supposed to use it in our projects. That is the only reason why I am uploading this, because 
+//      it will be seen in multiple projects for this class.
+
+
+
+
+// Import Statements
+
+import javax.swing.*;
+import java.awt.*;
+
+public class WordGUI{
+	//all the static values for the class
+	static JFrame myFrame;
+	static Container myContainer;
+	static TextArea sorted, notsorted;
+	
+
+	// this method initializes the JFrame
+	public static void initialize(){
+		myFrame = new JFrame();
+		myFrame.setSize(1000, 500);
+		myFrame.setLocation(100, 100);
+		myFrame.setTitle("Not Sorted and Sorted");
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setVisible(true);
+
+	}
+	
+	// this method adds content to the JFrame
+	public static void PrintToFrame(String [] Array, String [] Array1){
+		Container myContainer = myFrame.getContentPane();
+		TextArea sorted = new TextArea();
+		TextArea notsorted = new TextArea();
+		
+		myContainer.add(sorted, BorderLayout.EAST);
+			for(int i = 0; i < Array1.length; i++){
+				if(Array1[i].length() == 3){
+					sorted.append(Array1[i]+ "\n");
+				}
+			}
+		
+		myContainer.add(notsorted, BorderLayout.WEST);
+			for(int i = 0; i < Array.length; i++){
+				if(Array[i].length() == 3){
+					notsorted.append(Array[i]+"\n");
+				}
+			}
+			
+		myFrame.setVisible(true);
+
+	}
+
+}
